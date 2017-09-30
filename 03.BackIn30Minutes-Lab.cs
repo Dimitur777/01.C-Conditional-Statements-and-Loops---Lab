8c1facf9efc8a4e4eb748a9465682a180d4aeeb0
Dimitar Lyolev <lyolevofficial@gmail.com>
2017-09-30 11:40:58 +0300

    Add files via upload

---
+++ 03.BackIn30Minutes-Lab.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 30;
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes = minutes - 60;
+                
+                if (hours > 23)
+                {
+                    hours = 0;
+                }
+            }
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
